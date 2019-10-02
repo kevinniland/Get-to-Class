6a7cc4a22a63dc700ad900a266adb3e8d32ea77b
Kevin Niland <G00342279@gmit.ie>
2019-10-02 23:29:11 +0100

    Jump mechanic for player started

---
+++ Assets/__Scripts/Player Scripts/Jump.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Jump : MonoBehaviour {
+    public bool isGrounded;
+    public float jumpVelocity = 1000f;
+
+    [SerializeField]
+    private LayerMask groundLayerMask;
+
+    private Rigidbody2D rigidbody2D;
+    private BoxCollider2D boxCollider2D;
+    
+    // Start is called before the first frame update
+    void Start() {
+        rigidbody2D = transform.GetComponent<Rigidbody2D>();
+        boxCollider2D = transform.GetComponent<BoxCollider2D>();
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == ("Ground") && isGrounded == false) {
+            isGrounded = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space)) {
+            rigidbody2D.velocity = Vector2.up * jumpVelocity;
+        }
+    }
+
+    private bool IsGrounded() {
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down * .1f, groundLayerMask);
+
+        return raycastHit2D.collider != null; 
+    }
+}
