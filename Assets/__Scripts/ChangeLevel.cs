4fc35df45b90c0fa54f57924a844d98ed979e8ca
Kevin Niland <G00342279@gmit.ie>
2019-10-02 21:32:41 +0100

    Created ParentUtils script

---
+++ Assets/__Scripts/ChangeLevel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour
+{
+    void OnTriggerEnter(Collider LoadSceneTrigger) {
+        SceneManager.LoadScene("__Scenes/Level2");
+    }
+}
---
+++ Assets/__Scripts/ParentUtils.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utilities {
+    public class ParentUtils {
+        public const string ENEMY_PARENT = "EnemyParent";
+
+        public static GameObject GetEnemyParent() {
+            return GetParent(ENEMY_PARENT);
+        }
+
+        private static GameObject GetParent(string parentName) {
+            var parent = GameObject.Find(parentName);
+
+            if (!parent) {
+
+            }
+
+            return parent;
+        }
+    }
+}
