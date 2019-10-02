fdf68a36a972cc33e72222faf181819a10c369d9
Kevin Niland <G00342279@gmit.ie>
2019-10-02 21:40:41 +0100

    Created Enemy, EnemySpawn, and SpawnPoint scripts

---
+++ Assets/__Scripts/EnemySpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utilities;
+
+public class EnemySpawner : MonoBehaviour {
+    #region private variables
+    [SerializeField]
+    private Enemy enemyPrefab;
+    private float spawnDelay = 0.5f;
+    private float spawnInterval = 0.5f;
+
+    private IList<SpawnPoint> spawnPoints;
+
+    private GameObject enemyParent;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start() {
+        enemyParent = ParentUtils.GetEnemyParent();
+
+        // Need to get a list of spawn points
+        spawnPoints = GetComponentsInChildren<SpawnPoint>();
+
+        SpawnRepeating();
+    }
+
+    // invokeRepeating
+    private void SpawnRepeating() {
+        InvokeRepeating("Spawn", spawnDelay, spawnInterval);
+    }
+
+    private void Spawn() {
+        var randomIndex = Random.Range(0, spawnPoints.Count);
+        var currPoint = spawnPoints[randomIndex];
+
+        // var enemy = Instantiate(enemyPrefab); // Adds to the base level hierarchy
+        var enemy = Instantiate(enemyPrefab, enemyParent.transform);
+        enemy.transform.position = currPoint.transform.position;
+    }
+}
