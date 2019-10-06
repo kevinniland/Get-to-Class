8e309dec405547311c9fc0eda094ea71c5b2833f
Kevin Niland <G00342279@gmit.ie>
2019-10-06 14:29:23 +0100

    Able to change scenes

--- Assets/__Scripts/General Scripts/ChangeLevel.cs
+++ Assets/__Scripts/General Scripts/ChangeLevel.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeLevel : MonoBehaviour
-{
-    void OnTriggerEnter(Collider LoadSceneTrigger) {
-        SceneManager.LoadScene("__Scenes/Level2");
+public class ChangeLevel : MonoBehaviour {
+    #region private variables
+    [SerializeField]
+    private string nextLevel;
+    #endregion
+
+    void OnTriggerEnter2D(Collider2D LoadSceneTrigger) {
+        if (LoadSceneTrigger.CompareTag("Player")) {
+            SceneManager.LoadScene(nextLevel);
+        }
     }
 }
--- Assets/__Scripts/General Scripts/LockerSpawner.cs
+++ Assets/__Scripts/General Scripts/LockerSpawner.cs
@@ -6,17 +6,17 @@ using Utilities;
 public class LockerSpawner : MonoBehaviour {
     #region private variables
     [SerializeField]
-    private SingularLocker singularLocker; // Reference to the singular locker prefab
-    private GameObject lockerParent; // Reference to the enemy parent object
-    private int lockerCounter = 0; // Counter for enemies. It is incremented on enemy spawn
-    private int maxLockers = 50; // Determines maximum number of enemies that can be spawned
+    private Locker lockerPrefab; // Reference to the singular locker prefab
+    private GameObject lockerParent; // Reference to the locker parent object
+    private int lockerCounter = 0; // Counter for lockers. It is incremented on locker spawn
+    private int maxLockers = 100; // Determines maximum number of locker that can be spawned
 
-    private List<SingularLocker> lockers; // List of enemies
+    private List<Locker> lockers; // List of enemies
     #endregion
 
     // Start is called before the first frame update
     void Start() {
-        lockers = new List<SingularLocker>();
+        lockers = new List<Locker>();
 
         /* 
          * Using the ParentUtils class, get the locker parent object. An attempt to find the game object is made first.
@@ -28,12 +28,12 @@ public class LockerSpawner : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         if (lockerCounter < maxLockers) {
-            singularLocker.transform.position = new Vector2(Random.Range(0, 1000f), 0);
+            lockerPrefab.transform.position = new Vector2(Random.Range(0, 1000f), 0);
 
             // Instantiate each enemy based on the positions calculated above
-            Instantiate<SingularLocker>(singularLocker, lockerParent.transform);
+            Instantiate<Locker>(lockerPrefab, lockerParent.transform);
 
-            lockers.Add(singularLocker); // Add each enemy to the list
+            lockers.Add(lockerPrefab); // Add each enemy to the list
             lockerCounter++; // Increment the enemyCounter
         }
     }
