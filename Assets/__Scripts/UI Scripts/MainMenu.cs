a7e850ba2aca52ca5625d5238a13f8aa302c3692
Kevin Niland <G00342279@gmit.ie>
2019-10-06 19:35:33 +0100

    Main menu added

---
+++ Assets/__Scripts/UI Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    // In order to call this from the button, method needs to be public
+    public void PlayGame() {
+        // Loads next level in the queue
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Quits the game
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
