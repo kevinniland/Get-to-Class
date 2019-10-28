501569839dc7ddb491e3e5eeb1b05e224393f974
Kevin Niland <G00342279@gmit.ie>
2019-10-28 22:14:44 +0000

    Enemies can be destroyed by jumping on them

--- Assets/__Scripts/Enemy Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy Scripts/Enemy.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    #region private variables
+    private Rigidbody2D rigidbody2D;
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,11 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2D) {
+        if (collider2D.tag == "Player") {
+            Destroy(this.gameObject);
+        }
     }
 }
