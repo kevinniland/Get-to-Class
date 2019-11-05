0593076dfbb3d1be61e8ceac7c072a6220c94d01
Kevin Niland <G00342279@gmit.ie>
2019-11-05 11:03:34 +0000

    Fixed pause menu (stopped working previously)

--- Assets/__Scripts/Player Scripts/PlayerHealth.cs
+++ Assets/__Scripts/Player Scripts/PlayerHealth.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour {
     #region private variables
-    private Transform bar;
+    // private Transform bar;
     #endregion
 
     // Start is called before the first frame update
     private void Start() {
-        bar = transform.Find("Bar");
-        bar.localScale = new Vector3(1f, 1f);
+        //bar = transform.Find("Bar");
+        //bar.localScale = new Vector3(1f, 1f);
     }
 
-    public void SetSize(float sizeNormalized) {
-        bar.localScale = new Vector3(sizeNormalized, 1f);
-    }
+    //public void SetSize(float sizeNormalized) {
+    //    //bar.localScale = new Vector3(sizeNormalized, 1f);
+    //}
 }
